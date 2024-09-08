abb5b013ed4fc8f998b3fc363fac102c32a715fc
Büşra Tuğul <bsratugul@gmail.com>
2024-09-08 18:08:41 +0300

    Link componenti çalışıldı.

--- 01-routing/app/layout.js
+++ 01-routing/app/layout.js
@@ -1,7 +1,9 @@
+import '../output.css';
+
 export default function RootLayout({ children }) {
   return (
     <html lang="tr">
-      <body>
+      <body className='bg-gray-200 w-full p-10 text-center'>
         {children}
       </body>
     </html>
--- 01-routing/app/page.js
+++ 01-routing/app/page.js
@@ -1,5 +1,25 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
-   <div>Hello World</div>
-  );
+    <div className='flex flex-col'>
+      Hello World
+      <Link href="/hakkimizda" className="block text-blue-500">
+        Hakkımızda
+      </Link>
+      <Link href="./docs/post" className="text-pink-500">
+        Post 1
+      </Link>
+      <Link href={{
+        pathname:"/features/slug",
+        query: {
+          name: "Next.js",
+          surname: "ff",
+        }
+      }} 
+      className=" text-green-500">
+        Features
+      </Link>
+    </div>
+  )
 }
