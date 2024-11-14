8d198d6edaf5e7fe51de963c0f5281ccdd9a543a
Büşra <busra.tugul@yapp360.com.tr>
2024-11-14 08:48:17 +0300

    react-tekrar

--- 04-react-tekrar/src/main.jsx
+++ 04-react-tekrar/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import Posts from './routes/Posts.jsx'
+import Posts, { loader as postsLoader} from './routes/Posts.jsx'
 import NewPost from './routes/NewPost.jsx'
 import RootLayout from './routes/RootLayout.jsx'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
@@ -9,7 +9,7 @@ const router = createBrowserRouter([
     path: '/',
     element: <RootLayout />,
     children: [
-      { path: '/', element: <Posts />, children: [
+      { path: '/', element: <Posts />,loader: () => {}, children: [
         { path: '/create-post', element: <NewPost /> }
       ] }, //our main
     ],
